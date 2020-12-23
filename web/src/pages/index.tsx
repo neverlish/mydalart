c0a6151a262489d3c7ab7e7b9102a91857f827de
Jinho Hyeon <neverlish@gmail.com>
2020-12-23 23:37:06 +0900

    [web] called query on indexpage

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,6 +1,23 @@
+import { gql, useQuery } from "@apollo/client"
 import Head from 'next/head'
+import { PublicTaskList } from '../types/generated/PublicTaskList'
+
+const PUBLIST_TASK_LIST_QUERY = gql`
+  query PublicTaskList {
+    publicTaskList {
+      items {
+        id
+        text
+        user {
+          email
+        }
+      }
+    }
+  }
+`
 
 export default function Home() {
+  const { loading, error, data } = useQuery<PublicTaskList>(PUBLIST_TASK_LIST_QUERY)
   return (
     <div>
       <Head>
---
+++ web/src/types/generated/PublicTaskList.ts
@@ -0,0 +1,26 @@
+/* tslint:disable */
+/* eslint-disable */
+// @generated
+// This file was automatically generated and should not be edited.
+
+// ====================================================
+// GraphQL query operation: PublicTaskList
+// ====================================================
+
+export interface PublicTaskList_publicTaskList_items_user {
+  email: string;
+}
+
+export interface PublicTaskList_publicTaskList_items {
+  id: string;
+  text: string;
+  user: PublicTaskList_publicTaskList_items_user;
+}
+
+export interface PublicTaskList_publicTaskList {
+  items: PublicTaskList_publicTaskList_items[];
+}
+
+export interface PublicTaskList {
+  publicTaskList: PublicTaskList_publicTaskList;
+}
