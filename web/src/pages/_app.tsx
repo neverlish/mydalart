aa8448b5fb16cb072fbaa62cce9a9aef9ab25017
Jinho Hyeon <neverlish@gmail.com>
2020-12-25 01:36:25 +0900

    [web] changed default layout

--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -1,15 +1,28 @@
-import { ApolloProvider } from "@apollo/client"
-
-import { useApollo } from "../lib/apolloClient"
+import { UserOutlined } from '@ant-design/icons';
+import { ApolloProvider } from "@apollo/client";
+import { Layout } from 'antd';
+import 'antd/dist/antd.css';
+import Head from 'next/head';
+import { useApollo } from "../lib/apolloClient";
 
 export default function App({ Component, pageProps }) {
   const apolloClient = useApollo(pageProps.initialApolloState)
 
   return (
     <ApolloProvider client={apolloClient}>
-      <div style={{ margin: "20px" }}>
-        <Component {...pageProps} />
-      </div>
+      <Layout>
+        <Head>
+          <title>Mydalart</title>
+        </Head>
+        <Layout.Header style={{ textAlign: 'right' }}>
+          <UserOutlined style={{ color: '#fff', fontSize: '20px' }} />
+        </Layout.Header>
+        <Layout.Content style={{ padding: '48px' }}>
+          <Layout.Content style={{ padding: '24px', backgroundColor: '#fff' }}>
+            <Component {...pageProps} />
+          </Layout.Content>
+        </Layout.Content>
+      </Layout>
     </ApolloProvider>
   )
 }
